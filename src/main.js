4bc3c85d2e8ce850031ac73fb597a7d2d401c7c0
Your Name <you@example.com>
2024-08-04 00:14:53 +0200

    clean-up

--- src/main.js
+++ src/main.js
@@ -11,34 +11,11 @@ class Example extends Phaser.Scene
     create ()
     {
         const map = this.make.tilemap({ key: 'map' });
-
-        // The first parameter is the name of the tileset in Tiled and the second parameter is the key
-        // of the tileset image used when loading the file in preload.
+        
         const tiles = map.addTilesetImage('tileset', 'tiles');
 
-        // You can load a layer from the map using the layer name from Tiled, or by using the layer
-        // index (0 in this case).
         const layer = map.createLayer(0, tiles, 0, 0);
 
-        this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-
-        const cursors = this.input.keyboard.createCursorKeys();
-        const controlConfig = {
-            camera: this.cameras.main,
-            left: cursors.left,
-            right: cursors.right,
-            up: cursors.up,
-            down: cursors.down,
-            speed: 0.5
-        };
-        this.controls = new Phaser.Cameras.Controls.FixedKeyControl(controlConfig);
-
-        const help = this.add.text(16, 16, 'Arrow keys to scroll', {
-            fontSize: '18px',
-            padding: { x: 10, y: 5 },
-            backgroundColor: '#000000',
-            fill: '#ffffff'
-        });
         help.setScrollFactor(0);
     }
 
